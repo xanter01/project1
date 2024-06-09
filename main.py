687a9858e4b21057a21c37b1acfc3070abb9e6f4
xanter01 <137294715+xanter01@users.noreply.github.com>
2024-06-09 15:15:04 +0300

    Tkinter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 
 #сделать цикл по комбобоксу
+import os
+from datetime import datetime
+import webbrowser
+#import datetime
 from tkinter import *
 from tkinter import messagebox
 from tkinter.ttk import Combobox
@@ -7,58 +11,53 @@ from tkinter.ttk import Checkbutton
 import sqlite3
 
 global height_tf, weight_tf,geight_tf,peight_tf,\
-    oeight_tf,ueight_tf,combo,x,d,ii_tf,pi_tf,ui_tf,xi_tf,x2,userid,combo,a52,result1,ww,uu,window1,\
-    y,p,b,k,o,yy,h
+    oeight_tf,ueight_tf,combo,x,d,ii_tf,pi_tf,ui_tf,xi_tf,x2,userid,a52,result1,ww,uu,window1,\
+    y,p,b,k,o,yy,h,ddq,current_month,a53,label,aa
 
 #сделать запись кнопка добавить
 #сделать так чтобы окна сохранялись при закрытии
 def save_fail():
-    with open("new-file5.txt", "a") as jj:
-        ww = str(height_tf.get()) + ':' + str(weight_tf.get()) + ':' + str(geight_tf.get()) + ':' + str(
-            peight_tf.get()) + ':' + str(oeight_tf.get()) + ':' + str(ueight_tf.get()) + ':' + str(
-            combo.get()) + ':' + str(x.get()) + ':' + str(d.get()) + ':' + str(ii_tf.get()) + ':' + str(
-            pi_tf.get()) + ':' + str(ui_tf.get()) + ':' + str(xi_tf.get()) + '\n'
-        f = str(height_tf.get())  # фамилия
-        l = str(weight_tf.get())  # имя
-        q = str(geight_tf.get())  # очество
-        k = str(peight_tf.get())  # телефон 1
-        t = str(oeight_tf.get())  # телефон 2
-        u = str(ueight_tf.get())  # почта
-        s = int(combo.get())  # дата
-        v = int(x.get())  # месяц
-        m = int(d.get())  # год
-        y = str(ii_tf.get())  # город
-        r = str(pi_tf.get())  # улица
-        p = str(ui_tf.get())  # дом
-        o = str(xi_tf.get())  # квартира
+    f = str(height_tf.get())  # фамилия
+    l = str(weight_tf.get())  # имя
+    q = str(geight_tf.get())  # очество
+    k = str(peight_tf.get())  # телефон 1
+    t = str(oeight_tf.get())  # телефон 2
+    u = str(ueight_tf.get())  # почта
+    s = int(combo.get())  # дата
+    v = int(x.get())  # месяц
+    m = int(d.get())  # год
+    y = str(ii_tf.get())  # город
+    r = str(pi_tf.get())  # улица
+    p = str(ui_tf.get())  # дом
+    o = str(xi_tf.get())  # квартира
 
-        jj.write(ww)
-        #window.destroy()  # Закрыть окно
-        conn = sqlite3.connect('phoneboog.db')
-        c = conn.cursor()  # вставить м или ж
-        c.execute(
-            '''CREATE TABLE IF NOT EXISTS phonebook (id integer primary key,
-            surname text, 
-            name text,
-            patronymic text, 
-            phone1 text,
-            phone2 text DEFAULT ' ',
-            Email text,
-            date integer,
-            month integer,
-            year integer,
-            city text,
-            street text,
-            home text,
-            apartment text DEFAULT ' '
-            )
-            ''')
-        conn.commit()
+    #jj.write(ww)
+    #window.destroy()  # Закрыть окно
+    conn = sqlite3.connect('phoneboog.db')
+    c = conn.cursor()  # вставить м или ж
+    c.execute(
+        '''CREATE TABLE IF NOT EXISTS phonebook (id integer primary key,
+        surname text, 
+        name text,
+        patronymic text, 
+        phone1 text,
+        phone2 text DEFAULT ' ',
+        Email text,
+        date integer,
+        month integer,
+        year integer,
+        city text,
+        street text,
+        home text,
+        apartment text DEFAULT ' '
+        )
+        ''')
+    conn.commit()
 
-        c.execute(
-            '''INSERT INTO phonebook (surname, name, patronymic, phone1, phone2, Email, date, month, year, city, street, home, apartment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ''',
-            (f, l, q, k, t, u, s, v, m, y, r, p, o))
-        conn.commit()
+    c.execute(
+        '''INSERT INTO phonebook (surname, name, patronymic, phone1, phone2, Email, date, month, year, city, street, home, apartment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ''',
+        (f, l, q, k, t, u, s, v, m, y, r, p, o))
+    conn.commit()
 
             # Создание базы данных
         #print(ww)
@@ -269,7 +268,10 @@ def click():
     btn = Button(window, text="Сохранить",command=save_fail)
     btn.grid(column=2, row=11)
 
-    ee = Button(window, text="Выход")
+    def on_closing():
+        window.destroy()  # Закрыть окно
+
+    ee = Button(window, text="Выход",command=on_closing)
     ee.grid(column=3, row=11)
     #window.protocol(save_fail)#обработка закрытия окна
     window.mainloop()
@@ -277,7 +279,7 @@ def click():
 #окно редактор
 def redactor():
     #сделать используемые переменные глобальными
-    global combo
+    global ddq
     window = Tk()
     window.title("Новое окно")
     window.geometry("1920x1080")
@@ -293,7 +295,7 @@ def redactor():
     height_lb_tf.grid(row=0, column=1)
     con = sqlite3.connect("phoneboog.db")
     #получаем данные из базы данных
-    userid = combo.get()[0]
+    userid = ddq.get()[0]
     id = userid
     c = con.cursor()
     c.execute("SELECT surname FROM phonebook WHERE id = ?", (id,))
@@ -541,69 +543,326 @@ def redactor():
     btn = Button(window, text="Сохранить", command=save_fail)
     btn.grid(column=2, row=11)
 
-    ee = Button(window, text="Выход")
+    def on_closing():
+        window.destroy()
+
+    ee = Button(window, text="Выход",command=on_closing)
     ee.grid(column=3, row=11)
     # window.protocol(save_fail)#обработка закрытия окна
     window.mainloop()
 
 def search():
-    global a52,result1,ww,uu,window1,b,o
+    global a52,result1,ww,uu,window1,b,o,a53
     window1 = Tk()
     window1.title('База имён')
     window1.geometry('500x300')
     button = Button(window1, text="поиск",command=searchDB)
     button.grid(row=0, column=0)
+    def vuxod():
+        window1.destroy()
+    button1 = Button(window1, text="выход", command=vuxod)
+    button1.grid(row=0, column=1)
     y=Label(window1,text='фамилия')
     y.grid(row=1,column=0)
     a52 = Entry(window1)
     a52.grid(row=1, column=1)
+    y1 = Label(window1, text='имя')
+    y1.grid(row=2, column=0)
+    a53 = Entry(window1)
+    a53.grid(row=2, column=1)
+
+
 
 def searchDB():
-    global uu,window1
-    uu = str(a52.get())
+    global uu, window1
+    surname = str(a52.get())
+    name = str(a53.get())  # Предположим, что a53 - это поле для ввода имени
     con = sqlite3.connect("phoneboog.db")
     c1 = con.cursor()
-    c1.execute("SELECT * FROM phonebook WHERE surname=?", (uu,))
+
+    if surname and name:  # Если введены и фамилия, и имя
+        c1.execute("SELECT * FROM phonebook WHERE surname=? AND name=?", (surname, name))
+    elif surname:  # Если введена только фамилия
+        c1.execute("SELECT * FROM phonebook WHERE surname=?", (surname,))
+    elif name:  # Если введено только имя
+        c1.execute("SELECT * FROM phonebook WHERE name=?", (name,))
+    else:  # Если ничего не введено
+        messagebox.showinfo("Предупреждение", "Введите фамилию и/или имя для поиска")
+        return
+
     result = c1.fetchall()
     if result:
         if len(result) == 1:
             editor = Text(window1, height=30, width=50)
             editor.grid(row=4, column=1)
             editor.insert("1.0", result[0])
-            #messagebox.showinfo("Результат", f"Найдена запись: {result[0]}")
         else:
             message = "Найдены записи:\n"
             for row in result:
                 message += f"{row}\n"
-            #messagebox.showinfo("Результат", message)
             editor = Text(window1, height=30, width=50)
             editor.grid(row=4, column=1)
             editor.insert("1.0", message)
     else:
-        messagebox.showinfo("Результат", "Записей с такой фамилией не найдено")
+        messagebox.showinfo("Результат", "Записей с такой фамилией и/или именем не найдено")
 
-    #datetime.date если дата и месяц раны переменным в которых записаны эти данные то вывести инсерт сегодня др у наме и сурнами и отчество вывести
 
-aa = Tk()
-aa.title('База имён')
-aa.geometry('500x300')
-x1=Button(aa,text="добавить",command=click)
-x1.grid(row=0, column=0)
-x2=Button(aa,text="редактировать",command=redactor)
-x2.grid(row=0, column=1)
-x3=Button(aa,text="поиск",command=search)
-x3.grid(row=0, column=2)
-con = sqlite3.connect("phoneboog.db")
-cursor = con.cursor()
-#получаем данные из базы данных
-cursor.execute("SELECT id,surname,name, phone1 FROM phonebook")
-rows = cursor.fetchall()
-x=(rows)
-combo = Combobox(aa,values=x)
-combo.grid(column=0, row=1,pady=20,ipadx=50)
-print()
+def DR():
+    global current_month
+    con = sqlite3.connect("phoneboog.db")
+    c1 = con.cursor()
+    c1.execute("SELECT name,surname,patronymic,date,month,year FROM phonebook WHERE month=?", (current_month,))
+    result = c1.fetchall()
+    if result:
+        if len(result) == 1:
+            messagebox.showinfo("Результат", f"Найдена запись: {result[0]}")
+        else:
+            message = "Найдены записи:\n"
+            for row in result:
+                message += f"{row}\n"
+            messagebox.showinfo("Результат", message)
+    else:
+        messagebox.showinfo("Результат", "в этом месяце др не найдено")
 
-aa.mainloop()
-#задать каждой команду открывать прошлый код и если добавить то инпут
+def update_time():
+    global label,aa
+    label.config(text=f"{datetime.now():%D   %H:%M:%S}")
+    aa.after(100, update_time)  # Запланировать выполнение этой же функции через 100 миллисекунд
+
+def rt():
+    global ddq,current_month,aa,label
+    aa = Tk()
+    #aa["bg"] = "#DCDCDC"
+    aa.title('информационное окно')
+    aa.geometry('600x400')
+    label =Label(aa, font=("helvetica", 15),foreground='#9ACD32')
+    label.grid(row=0,column=0)
+
+    update_time()
+
+    x1=Button(aa,text="добавить",command=click)
+    x1.grid(row=1, column=0)
+    x2=Button(aa,text="редактировать",command=redactor)
+    x2.grid(row=0, column=1)
+
+    def hhh():
+        import csv
+
+        # Подключение к базе данных
+        conn = sqlite3.connect('phoneboog.db')
+        cursor = conn.cursor()
+
+        # Выполнение запроса к базе данных
+        cursor.execute("SELECT * FROM phonebook")
+
+        # Получение результатов запроса и названий столбцов
+        rows = cursor.fetchall()
+        column_names = [description[0] for description in cursor.description]
+
+        # Запись результатов в CSV файл с названиями столбцов
+        with open('human.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(column_names)  # записываем названия столбцов
+            writer.writerows(rows)  # записываем данные
+
+        # Закрытие соединения с базой данных
+        conn.close()
+
+    pechat=Button(aa,text="записать в файл",command=hhh)
+    pechat.grid(row=0, column=2)
+    x3=Button(aa,text="поиск",command=search)
+    x3.grid(row=1, column=1)
+
+    def yuy():
+        if os.name=='nt':
+            import win32com.client
+
+            # Создание экземпляра приложения Excel
+            excel = win32com.client.Dispatch("Excel.Application")
+
+            # Открытие файла
+            workbook = excel.Workbooks.Open(r'путь_к_вашему_файлу.xlsx')
+
+            # Печать файла
+            workbook.PrintOut()
+
+            # Закрытие файла и приложения Excel
+            workbook.Close(SaveChanges=False)
+            excel.Quit()
+
+        import subprocess
+
+        def print_csv_on_mac(file_path):
+            applescript = f'''
+            tell application "Finder"
+                set theFile to POSIX file "{file_path}" as alias
+                print theFile
+            end tell
+            '''
+
+            process = subprocess.Popen(['osascript', '-e', applescript], stdout=subprocess.PIPE)
+            output, _ = process.communicate()
+            if output:
+                print(output.decode('utf-8'))
+
+        # Укажите путь к вашему CSV файлу
+        csv_file_path = "/Users/artemabasov/PycharmProjects/всплывыющее окно/human.csv"
+
+        print_csv_on_mac(csv_file_path)
+
+    ll = Button(aa, text="печать", command=yuy)
+    ll.grid(row=1, column=2)
+    x4=Button(aa,text="день рождения",command=DR)
+    x4.grid(row=2,column=1)
+
+
+    def web():
+        webbrowser.open_new('https://www.youtube.com/?app=desktop&hl=ru')
+
+    x5=Button(aa,text="ютуб",command=web,foreground='red')
+    x5.grid(row=2,column=2)
+
+    con = sqlite3.connect("phoneboog.db")
+    cursor = con.cursor()
+    #получаем данные из базы данных
+    cursor.execute("SELECT id,surname,name, phone1 FROM phonebook")
+    rows = cursor.fetchall()
+    x=rows
+
+    ddq= Combobox(aa,values=x)
+    ddq.grid(column=0, row=2,pady=20,ipadx=50)
+
+    current_datetime = datetime.now()
+    current_month = current_datetime.month
+
+    aa.mainloop()
+
+
+
+global entry_login4 , entry_password3,registrator
+
+def login():
+    global entry_login4 , entry_password3,registrator
+    qq = str(entry_login4.get())
+    gg = str(entry_password3.get())
+    if qq=='' or gg=='':
+        messagebox.showinfo("Результат", "вы ничего не ввели")
+    elif gg!='' and gg!='':
+        rek = sqlite3.connect('phoneboog.db')
+
+        hh = rek.cursor()
+        hh.execute('''CREATE TABLE IF NOT EXISTS users
+                        (id INTEGER PRIMARY KEY, login TEXT, password TEXT)''')
+        rek.commit()
+        hh.execute(
+            '''INSERT INTO users (login,password) VALUES (?, ?) ''',
+            (qq,gg))
+        rek.commit()
+        rt()
+
+def registr():
+    global entry_login4, entry_password3
+    def showpass():
+        if entry_password3['show'] == '*':
+            entry_password3['show'] = ''
+        else:
+            entry_password3['show'] = '*'
+
+    vxod.destroy()
+    global registrator
+    registrator = Tk()
+    registrator.title('регистрация')
+    registrator.geometry('270x260')
+
+    login2 = Label(registrator, text='Login:', foreground='#8370D8')
+    login2.grid(row=0, column=1)
+
+    entry_login2 = Entry(registrator)
+    entry_login2.grid(row=1, column=1)
+
+    login3 = Label(registrator, text='повторить Login:', foreground='#8370D8')
+    login3.grid(row=2, column=1)
+
+    entry_login4 = Entry(registrator)
+    entry_login4.grid(row=3, column=1)
+
+    password2 = Label(registrator, text='Password:', foreground='#8370D8')
+    password2.grid(row=4, column=1)
+
+    entry_password2 = Entry(registrator, show='*')
+    entry_password2.grid(row=5, column=1)
+
+    password3 = Label(registrator, text='повторить Password:', foreground='#8370D8')
+    password3.grid(row=6, column=1)
+
+    entry_password3 = Entry(registrator, show='*')
+    entry_password3.grid(row=7, column=1)
+
+    show2 = Button(registrator, text='show', foreground='#8370D8', command=showpass)
+    show2.grid(row=7, column=2)
+
+    signin2 = Button(registrator, text='Sign in', foreground='#8370D8', command=login)
+    signin2.grid(row=9, column=1)
+
+
+
+def showpass():
+    if entry_password1['show'] == '*':
+        entry_password1['show'] = ''
+    else:
+        entry_password1['show'] = '*'
+def vvod():
+    global entry_login1,entry_password1
+    s = str(entry_login1.get())
+    q=str(entry_password1.get())
+    con = sqlite3.connect("phoneboog.db")
+    c1 = con.cursor()
+    c1.execute("SELECT * FROM users WHERE login=?", (s,))
+    result = c1.fetchall()
+    if s=='' or q=='':
+        messagebox.showinfo("Результат", "вы ничего не ввели")
+    elif len(result)==0:
+        messagebox.showinfo("Результат", "неверные данные для входа")
+    else:
+        f=result[0][2]
+        if q==f:
+            rt()
+        elif q!=f:
+            messagebox.showinfo("Результат", "неверные данные для входа")
+
+
+
+vxod = Tk()
+vxod.title('Авторазиция')
+vxod.geometry('260x160')
+
+login1=Label(vxod, text='Login:', foreground='#8370D8')
+login1.grid(row=0,column=1)
+
+entry_login1 = Entry(vxod)
+entry_login1.grid(row=1,column=1)
+
+
+password1=Label(vxod, text='Password:', foreground='#8370D8')
+password1.grid(row=2,column=1)
+
+entry_password1 = Entry(vxod, show='*')
+entry_password1.grid(row=3,column=1)
+
+
+show1=Button(vxod, text='show',foreground='#8370D8', command=showpass)
+show1.grid(row=3,column=2)
+
+signin1=Button(vxod, text='Sign in',foreground='#8370D8' , command=vvod)#rt)
+signin1.grid(row=4,column=1)
+
+req1=Button(vxod, text='зарегестрироваться',foreground='#8370D8' , command=registr)
+req1.grid(row=5,column=1)
+
+vxod.mainloop()
+
+
+
+#update в редактироать
 
 
